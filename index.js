418499c5c23d7faeb9c5aab163f19a43ef92a420
eslamheshamm <esl2mhesham@gmail.com>
2020-12-15 20:17:53 +0200

    creating index and add variables

---
+++ index.js
@@ -0,0 +1,12 @@
+// Declearing Variable
+var x;
+// Assign value to variable
+x = "Hi";
+// Declearing & Assigning at the same time
+var y = "World";
+// Datatypes
+var a = "AAAA"; // String
+var b = 2; // Number
+var c = true; // Boolean
+var d = ["hi", "world", "learn", 1, true];
+var e = { firstName: "Eslam", lastName: "Hesham", age: 21 };
