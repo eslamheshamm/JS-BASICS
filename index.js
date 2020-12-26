93a08c55f5cb9b6d57e00e57833c8c5eea61787f
eslamheshamm <esl2mhesham@gmail.com>
2020-12-26 23:25:16 +0200

    describe what is iteration, change variables to ES06

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // Declearing Variable
-var x;
+// Declares a variable, optionally initializing it to a value.
+let x;
 // Assign value to variable
 x = "Hi";
 // Declearing & Assigning at the same time
-var y = "World";
+const y = "World";
 // Datatypes
-var a = "AAAA"; // String
-var b = 2; // Number
-var c = true; // Boolean
-var d = ["hi", "world", "learn", 1, true];
-var e = { firstName: "Eslam", lastName: "Hesham", age: 21 };
+const a = "AAAA"; // String
+const b = 2; // Number
+const c = true; // Boolean
+const d = ["hi", "world", "learn", 1, true];
+const e = { firstName: "Eslam", lastName: "Hesham", age: 21 };
+// ES06 and the new way to decleare variable
+// Declares a block-scoped, local variable, optionally initializing it to a value.
+const z = "Eslam";
+// Declares a block-scoped, read-only named constant.
+const xx = "Hesham";
+
+// iteration vs recurasion
+// iteration: impartive - looping - stateful
+const sum = (numbers) => {
+	let total = 0;
+	for (i = 0; i < numbers.length; i++) {
+		total += numbers[i];
+	}
+	return total;
+};
+sum([0, 2, 4]); // 6
+// recursion: functional - self-referential stateless
